7e66f614137381b469f76cd5bf47b433bc0bd9e0
naeem <naeemislamjr182@gmail.com>
2023-08-31 00:06:29 +0600

    show all btn conditionally add or hidden and perfectly done

--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,14 @@
-const loadphone = async (searchtext) => {
+const loadphone = async (searchtext, isshowall) => {
 
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchtext}`);
 
     const data = await res.json();
     const phones = data.data
     // console.log(data.data)
-    displayPhones(phones)
+    displayPhones(phones, isshowall)
 
 }
-const displayPhones = phones => {
+const displayPhones = (phones, isshowall) => {
 
     const phonecontainer = document.getElementById('phone_container')
     //clear data
@@ -17,15 +17,17 @@ const displayPhones = phones => {
     //display show all btn if card name is getter than 10
 
     const showallContainer = document.getElementById('show-all-container')
-    if (phones.length > 10) {
+    if (phones.length > 10 && !isshowall) {
         showallContainer.classList.remove('hidden')
     }
     else {
         showallContainer.classList.add('hidden')
     }
-    //display only first 10 phones
-    phones = phones.slice(0, 10);
-
+    console.log('isshowall', isshowall)
+    //display only first 10 phones if not show all
+    if (!isshowall) {
+        phones = phones.slice(0, 10);
+    }
 
     phones.forEach(phone => {
         console.log(phone);
@@ -47,15 +49,37 @@ const displayPhones = phones => {
         phonecontainer.appendChild(phoneCard)
 
     });
+    toggleloadingspinner(false)
 
 }
 
 //handel search
-const HandleSearch = () => {
-
+const HandleSearch = (isshowall) => {
+    //true loading
+    toggleloadingspinner(true);
     const searchfield = document.getElementById('searchfield')
     const searchtext = searchfield.value
 
     console.log(searchtext)
-    loadphone(searchtext);
+    loadphone(searchtext, isshowall);
+}
+//spinner hide and open
+function toggleloadingspinner(isloading) {
+
+    const loadingspinner = document.getElementById('loading-spinner');
+
+    if (isloading) {
+        loadingspinner.classList.remove('hidden');
+    }
+    else {
+        loadingspinner.classList.add('hidden');
+    }
+}
+
+
+
+
+///show all handler+
+const handleShowall = () => {
+    HandleSearch(true);
 }
