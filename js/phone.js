f1a8ea48e68c7c5edd6bb0e01701f9aee03a2c4c
naeem <naeemislamjr182@gmail.com>
2023-08-31 01:59:01 +0600

    show details

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadphone = async (searchtext, isshowall) => {
+const loadphone = async (searchtext = 13, isshowall) => {
 
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchtext}`);
 
@@ -23,14 +23,14 @@ const displayPhones = (phones, isshowall) => {
     else {
         showallContainer.classList.add('hidden')
     }
-    console.log('isshowall', isshowall)
+    //console.log('isshowall', isshowall)
     //display only first 10 phones if not show all
     if (!isshowall) {
         phones = phones.slice(0, 10);
     }
 
     phones.forEach(phone => {
-        console.log(phone);
+        //  console.log(phone);
         //create a div
         const phoneCard = document.createElement('div')
         phoneCard.classList = "border-4  card p-6  bg-gray  "
@@ -40,8 +40,9 @@ const displayPhones = (phones, isshowall) => {
             <h2 class="card-title">${phone.phone_name
             }</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button onclick="handleshowdetails('${phone.slug}');
+                my_modal_1.showModal()" class="btn btn-primary">Show details</button>
             </div>
         </div>
         `
@@ -60,7 +61,7 @@ const HandleSearch = (isshowall) => {
     const searchfield = document.getElementById('searchfield')
     const searchtext = searchfield.value
 
-    console.log(searchtext)
+    // console.log(searchtext)
     loadphone(searchtext, isshowall);
 }
 //spinner hide and open
@@ -76,10 +77,28 @@ function toggleloadingspinner(isloading) {
     }
 }
 
+//
+const handleshowdetails = async (id) => {
+    // console.log('click show')
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
 
+    const data = await res.json();
+    const phone = data.data
+    // console.log(phones)
+    showphonedetails(phone)
+
+}
+const showphonedetails = (phone) => {
+
+    // console.log(phone);
+    const showdetailsphonename = document.getElementById('showdetailsphonename')
+    showdetailsphonename.innerText = phone.name
+
+}
 
 
 ///show all handler+
 const handleShowall = () => {
     HandleSearch(true);
 }
+loadphone();
