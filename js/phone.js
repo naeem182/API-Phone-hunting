345c63a42e471d6349553248f6f1719556a7fa52
naeem <naeemislamjr182@gmail.com>
2023-08-30 22:04:04 +0600

    dynamic search by using handler and api

--- js/phone.js
+++ js/phone.js
@@ -1,9 +1,48 @@
-const loadphone = async () => {
+const loadphone = async (searchtext) => {
 
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchtext}`);
 
     const data = await res.json();
-    console.log(data.data)
+    const phones = data.data
+    // console.log(data.data)
+    displayPhones(phones)
 
 }
-loadphone();
+const displayPhones = phones => {
+
+    const phonecontainer = document.getElementById('phone_ontainer')
+    //clear data
+    phonecontainer.textContent = ''
+
+    phones.forEach(phone => {
+        console.log(phone);
+        //create a div
+        const phoneCard = document.createElement('div')
+        phoneCard.classList = "border-4  card p-6  bg-gray  "
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name
+            }</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+
+        phonecontainer.appendChild(phoneCard)
+
+    });
+
+}
+
+//handel search
+const HandleSearch = () => {
+
+    const searchfield = document.getElementById('searchfield')
+    const searchtext = searchfield.value
+
+    console.log(searchtext)
+    loadphone(searchtext);
+}
